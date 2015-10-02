38f4b39df140df35daeae6d04586d20745a3bfdd
Pavol Senko <pavol.senko@gmail.com>
2015-10-02 19:02:04 +0200

    update + refactor

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,9 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
-use App\Http\Controllers\Controller;
 
-class articleController extends Controller
+class ArticleController extends Controller
 {
 
     public function index()
