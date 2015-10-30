d9e0681f7281323abd01b7251b27a394a3bb72d5
Pavol Senko <pavol.senko@gmail.com>
2015-10-30 20:36:26 +0100

    blog

--- resources/views/admin/blogPosts/createOrUpdate.blade.php
+++ resources/views/admin/blogPosts/createOrUpdate.blade.php
@@ -32,42 +32,98 @@
             </div>
         @endif
 
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <div class="pull-right">
-                    @if(isset($blog_post))
-                        @lang('blog.id'): {{ $blog_post['id'] }} |
-                        @lang('blog.created_at'): {{ $blog_post['created_at'] }} |
-                        @lang('blog.last_updated_at'): {{ $blog_post['updated_at'] }}
-                    @endif
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <div class="pull-right">
+                            @if(isset($blog_post))
+                                @lang('blog.id'): {{ $blog_post['id'] }} |
+                                @lang('blog.created_at'): {{ $blog_post['created_at'] }} |
+                                @lang('blog.last_updated_at'): {{ $blog_post['updated_at'] }}
+                            @endif
+                        </div>
+                        @include('admin/blogPosts/toolbar')
+                    </div>
+
+                    <div class="panel-body">
+                        <div class="form-group">
+                            {!! Form::label('title', trans('blog.title')) !!}
+                            {!! Form::text('title', null, ['class' => 'form-control']) !!}
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('intro_text', trans('blog.intro_text')) !!}
+                            {!! Form::textarea('intro_text', null, ['class' => 'form-control editable']) !!}
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('body_text', trans('blog.body_text')) !!}
+                            {!! Form::textarea('body_text', null, ['class' => 'form-control editable']) !!}
+                        </div>
+                    </div>
+
+                    <div class="panel-footer">
+                        @include('admin/blogPosts/toolbar')
+                    </div>
+
                 </div>
-                @include('admin/blogPosts/toolbar')
             </div>
-
-            <div class="panel-body">
-                <div class="form-group">
-                    {!! Form::label('title', trans('blog.title')) !!}
-                    {!! Form::text('title', null, ['class' => 'form-control']) !!}
-                </div>
-
-                <div class="form-group">
-                    {!! Form::label('intro_text', trans('blog.intro_text')) !!}
-                    {!! Form::textarea('intro_text', null, ['class' => 'form-control editable']) !!}
-                </div>
-
-                <div class="form-group">
-                    {!! Form::label('body_text', trans('blog.body_text')) !!}
-                    {!! Form::textarea('body_text', null, ['class' => 'form-control editable']) !!}
-                </div>
-            </div>
-
-            <div class="panel-footer">
-                @include('admin/blogPosts/toolbar')
-            </div>
-
         </div>
 
-        <h4>@lang('comment.comments')</h4>
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <b>@lang('blog.category')</b>
+                    </div>
+                    <div class="panel-body">
+                        <div class="form-group">
+                            {!! Form::select('categories[]', $categories, $selected_categories, ['class' => 'form-control', 'multiple' => true, 'id' => 'category-multiselect']) !!}
+                        </div>
+                    </div>
+                    <div class="panel-footer">
+                        @include('admin/blogPosts/toolbar')
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <b>@lang('blog.author')</b>
+                    </div>
+                    <div class="panel-body">
+                        <div class="form-group">
+                            {!! Form::label('author', trans('blog.author')) !!}
+                            {!! Form::select('author', $authors, isset($blog_post) ? $blog_post['author_id'] : null, ['class' => 'form-control']) !!}
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::submit(trans('blog.save'), ['class' => 'btn btn-primary btn-sm']) !!}
+                            <a href="#" class="btn btn-default btn-sm">@lang('blog.manage_authors')</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <b>@lang('comment.comments')</b>
+                    </div>
+                    <div class="panel-body">
+                        lorem upsum
+
+                    </div>
+                    <div class="panel-footer">
+                        @include('admin/blogPosts/toolbar')
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
 
@@ -82,21 +138,6 @@
                         <a href="#" class="btn btn-default">@lang('blog.published')</a>
                         <a href="#" class="btn btn-default">@lang('blog.draft')</a>
                     </div>
-                </div>
-
-                <div class="form-group">
-                    {!! Form::label('author', trans('blog.author')) !!}
-                    {!! Form::select('author', $authors, isset($blog_post) ? $blog_post['author_id'] : null, ['class' => 'form-control']) !!}
-                </div>
-
-                <div class="form-group">
-                    {!! Form::label('categories', trans('blog.category')) !!}
-                    {!! Form::select('categories[]', $categories, $selected_categories, ['class' => 'form-control', 'multiple' => true, 'id' => 'category-multiselect']) !!}
-                </div>
-
-                <div class="form-group">
-                    {!! Form::submit(trans('blog.save'), ['class' => 'btn btn-primary btn-sm']) !!}
-                    <a href="#" class="btn btn-default btn-sm">@lang('blog.manage_authors')</a>
                 </div>
             </div>
         </div>
@@ -119,22 +160,18 @@
                     {!! Form::label('url', trans('blog.url')) !!}
                     {!! Form::text('url', null, ['class' => 'form-control']) !!}
                 </div>
-
                 <div class="form-group">
                     {!! Form::label('meta_title', trans('blog.meta_title')) !!}
                     {!! Form::text('meta_title', null, ['class' => 'form-control']) !!}
                 </div>
-
                 <div class="form-group">
                     {!! Form::label('meta_description', trans('blog.meta_description')) !!}
                     {!! Form::textarea('meta_description', null, ['class' => 'form-control']) !!}
                 </div>
-
                 <div class="form-group">
                     {!! Form::label('meta_keywords', trans('blog.meta_keywords')) !!}
                     {!! Form::textarea('meta_keywords', null, ['class' => 'form-control']) !!}
                 </div>
-
             </div>
         </div>
     </div>
